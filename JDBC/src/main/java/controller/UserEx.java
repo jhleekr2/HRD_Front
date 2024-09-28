6e0ff5f57d8669edc897a0eadf7917526e480618
jhleekr2 <jhleekr2@hanmail.net>
2024-09-29 02:43:13 +0900

    20240929

---
+++ JDBC/src/main/java/controller/UserEx.java
@@ -0,0 +1,30 @@
+package controller;
+
+public class UserEx {
+
+	// DAO 생성
+//	private UserDao userDao = null; // 동작되도록 바꾸세요!!
+	
+	public static void main(String[] args) {
+		
+		//--- 전체 출력 ---
+		
+		//--- 유저 삽입 ---
+		// 유저 객체를 생성하고
+		// 유저 정보를 객체에 입력하고
+		//DB에 삽입되도록 구현하세요!!
+				
+		//	userId : A123
+		//	name : Alice
+		
+		
+		//--- 유저 조회 ---
+		
+		//--- 유저 삭제 ---
+		
+		
+		// 유저 조회 - 삭제됐는지 확인하는 용도(null나와야함)
+		
+		
+	}
+}
